27109bbc3930c0d6afa80f47cb0ecbab909076a5
allofgame26 <rizkiarya79@yahoo.com>
2024-11-06 11:35:23 +0700

    migration done

--- database/migrations/2024_11_05_131905_t_tagging_bd_sertifikasi.php
+++ database/migrations/2024_11_05_131905_t_tagging_bd_sertifikasi.php
@@ -11,7 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('t_tagging_bd_sertifikasi', function (Blueprint $table){
+            $table->id('id_tagging_bd');
+            $table->unsignedBigInteger('id_sertifikasi')->index();
+            $table->unsignedBigInteger('id_bd')->index();
+            $table->timestamps();
+            
+            $table->foreign('id_sertifikasi')->references('id_sertifikasi')->on('m_sertifikasi');
+            $table->foreign('id_bd')->references('id_bd')->on('m_bidang_minat');
+        });
     }
 
     /**
@@ -19,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('t_tagging_bd_sertifikasi');
     }
 };
